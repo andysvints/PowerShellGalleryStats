6b90f4afb16264c1df0774fcdfce705b5de5e8ea
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2025-09-23 15:18:27 -0400

    Fix error handling by adding output stream write

--- ContainerizedWebApp/Microservice.ps1
+++ ContainerizedWebApp/Microservice.ps1
@@ -115,8 +115,9 @@ Register-EngineEvent -SourceIdentifier HTTP.Request -Action {
             $response.StatusCode = 404
             $errorFilePath = Join-Path -Path "$PSScriptRoot\Web" -ChildPath "index.html"
             $outputBuffer = [System.IO.File]::ReadAllBytes($errorFilePath)
-            $response.OutputStream.Write($outputBuffer, 0, $outputBuffer.Length)
             $response.RedirectLocation = "/index.html#t5"
+            $response.OutputStream.Write($outputBuffer, 0, $outputBuffer.Length)
+            
         }
         $response.Close()
     } catch {
