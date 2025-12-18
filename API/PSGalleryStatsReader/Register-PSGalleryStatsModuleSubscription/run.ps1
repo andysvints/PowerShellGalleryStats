530419873e1ed0546f31dad42bbce6ece30399dd
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2025-12-18 14:47:05 -0500

    Update run.ps1

--- API/PSGalleryStatsReader/Register-PSGalleryStatsModuleSubscription/run.ps1
+++ API/PSGalleryStatsReader/Register-PSGalleryStatsModuleSubscription/run.ps1
@@ -3,23 +3,18 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-# Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
-
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
+$statusCode = [HttpStatusCode]::OK
+$body = @{
+    ok = $true
 }
 
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-
-if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
-}
+ if ([string]::IsNullOrWhiteSpace($Request.Body)) {
+        $statusCode = [HttpStatusCode]::BadRequest
+        $body = @{ ok = $false; error = "Missing request body. Expected JSON: { email, moduleId }." }
+    }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
+    StatusCode = $statusCode
     Body = $body
 })
