ead6c66f5ff48f90a9e13d6225ff2de67eb45a6d
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2025-12-30 11:43:20 +0200

    Update email sender address to use environment variable

--- API/PSGalleryStatsReader/Send-PSGalleryStatsModuleNotification/run.ps1
+++ API/PSGalleryStatsReader/Send-PSGalleryStatsModuleNotification/run.ps1
@@ -1,7 +1,14 @@
 # Input bindings are passed in via param block.
 param($Timer)
 
+$ct = [System.Threading.CancellationToken]::None
+$now = (Get-Date).ToUniversalTime().ToString("o")
+
 Import-Module -Name Az.Communication
+
+$ctx = New-AzStorageContext -StorageAccountName $env:SUBSCRIPTIONS_STORAGE_ACCOUNT -UseConnectedAccount -Endpoint "core.windows.net"
+$storageTable = Get-AzStorageTable -Name $($env:SUBSCRIPTIONS_TABLE_NAME) -Context $ctx
+
 $to = @(
     @{
         Address = $env:emailRecipientTo
@@ -12,7 +19,7 @@ $to = @(
 $message = @{
     ContentSubject = "Test Email"
     RecipientTo = $to 
-    SenderAddress = 'donotreply@stats.psfundamentals.com'   
+    SenderAddress = $($env:SenderAddress) 
     ContentHtml = "<html><head><title>Enter title</title></head><body><img src='cid:inline-attachment' alt='Company Logo'/><h1>This is the first email from ACS - Azure PowerShell</h1></body></html>"
     ContentPlainText = "This is the first email from ACS - Azure PowerShell"  
 }
