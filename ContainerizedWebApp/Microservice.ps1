a4588c807cc069212415793b9d679ed344bb28e0
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2026-02-06 15:56:00 +0200

    Add community impact template handling

--- ContainerizedWebApp/Microservice.ps1
+++ ContainerizedWebApp/Microservice.ps1
@@ -78,6 +78,10 @@ Register-EngineEvent -SourceIdentifier HTTP.Request -Action {
             
             Get-PSTopModule -Top 10
         }
+        if($IndexPageHTML -match "<CommunityImpactTemplate>" ){
+            
+            Get-CommunityImpact.ps1
+        }
         
         # Forget favicons.
         if ($request.Url.LocalPath -eq '/favicon.ico') {
