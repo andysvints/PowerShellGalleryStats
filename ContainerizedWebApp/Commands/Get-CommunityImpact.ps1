ee6289751d2978e1b10ca91d31471ed464de8d68
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2026-02-06 15:54:42 +0200

    Add Get-CommunityImpact cmdlet for community data

---
+++ ContainerizedWebApp/Commands/Get-CommunityImpact.ps1
@@ -0,0 +1,42 @@
+function Get-CommunityImpact
+{
+    <#
+    .Synopsis
+       Short description
+    .DESCRIPTION
+       Long description
+    .EXAMPLE
+       Example of how to use this cmdlet
+    #>
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                  ConfirmImpact='Medium')]
+    [Alias()]
+    Param
+    ()
+
+    Begin
+    {
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess(""))
+        {
+            $ModulesFilePath = Join-Path -Path "/usr/local/share/powershell/Modules/PSGalleryModuleScore" -ChildPath "CommunityImpact.json"
+            Write-Verbose "Community impact data path - $ModulesFilePath"
+            $CommunityImpact=Get-Content $ModulesFilePath | ConvertFrom-Json -Depth 10
+            $IndexPageHTML=Get-Content $(Join-Path -Path "/usr/local/share/powershell/Modules/PSGalleryModuleScore/Web" -ChildPath "index.html")
+            #Generate HTML 
+            $HTMLTable=[System.Text.StringBuilder]::new()
+            $HTMLTable.AppendLine("
+            Community impact: $($CommunityImpact.PRsSubmitted)+ PRs, $($CommunityImpact.IssuesOpened)+ issues · Updated daily <br>
+<a href=`"https://github.com/search?q=stats.psfundamentals.com&type=pullrequests`" target=`"_blank`">See Details</a>
+            ")
+            #Inject Into index.html
+            $IndexPageHTML=$IndexPageHTML.Replace("<CommunityImpactTemplate>",$($HTMLTable.ToString())) 
+            
+        }
+    }
+    End
+    {
+    }
+}
