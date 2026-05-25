71326c46315dfe5e5c4782a51b6591eb3e466398
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2026-05-25 09:34:57 +0300

    Update query sanitization in search request

--- ContainerizedWebApp/Microservice.ps1
+++ ContainerizedWebApp/Microservice.ps1
@@ -87,7 +87,7 @@ Register-EngineEvent -SourceIdentifier HTTP.Request -Action {
         }
 
         if ($request.Url.LocalPath -eq '/search' -and $request.HttpMethod -eq 'GET') {
-            $query = $request.QueryString['query']-replace '[^a-zA-Z0-9.-]', ''
+            $query = $request.QueryString['query']-replace '[^a-zA-Z0-9._-]', ''
 
             $html  = Get-PSModuleInfo -Query $query
             $bytes = [Text.Encoding]::UTF8.GetBytes($html)
